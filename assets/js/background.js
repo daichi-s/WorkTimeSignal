aca42f702800756242bbec00efcdbfabdfdbefd5
塩飽 大地 <d-shiwaku@creative-innovation.co.jp>
2020-06-12 02:54:55 +0900

    設定時刻を過ぎていた場合は時報をセットしない、日付が変わる時にリロードするように修正

--- assets/js/background.js
+++ assets/js/background.js
@@ -9,26 +9,51 @@ const toDay = toMonth + "-" + date.getDate();
 const lunchTime = new Date(year + "-" + toDay + " 13:00:00");
 // 定時時刻
 const regularTime = new Date(year + "-" + toDay + " 19:00:00");
+// リセット
+const resetTime = new Date(year + "-" + toDay + " 23:59:59");
 
+/**
+ * 定期実行を設定
+ */
 chrome.runtime.onInstalled.addListener(() => {
-    // お昼休憩の時報を設定
-    chrome.alarms.create('lunch', { when: lunchTime.getTime() });
-    // 定時時刻に時報を設定
-    chrome.alarms.create('regular', { when: regularTime.getTime() });
+    // 現在時刻
+    const nowTime = new Date();
+
+    // 設定時刻を過ぎていた場合、時報をセットしない
+    if (lunchTime > nowTime) {
+        // お昼休憩の時報を設定
+        chrome.alarms.create('lunch', { when: lunchTime.getTime() });
+    }
+    if (regularTime > nowTime) {
+        // 定時時刻に時報を設定
+        chrome.alarms.create('regular', { when: regularTime.getTime() });
+    }
+    if (resetTime > nowTime) {
+        // 翌日の時報をセット
+        chrome.alarms.create('resetAlarms', { when: resetTime.getTime() });
+    }
 });
 
 /**
  * 時報を実行
+ * 
+ * @param {object} alarm
  */
 chrome.alarms.onAlarm.addListener((alarm) => {
     let audioSrcPath;
+
     if (alarm.name === 'lunch') {
         let audioSrcPath = './mp3/speech_lunch_break_time.mp3';
         playAudio(audioSrcPath);
-
-    } else if (alarm.name === 'regular') {
+    }
+    if (alarm.name === 'regular') {
         let audioSrcPath = './mp3/speech_regular_time.mp3';
         playAudio(audioSrcPath);
+    }
+
+    // 再読み込み
+    if (alarm.name === 'resetAlarms') {
+        chrome.runtime.reload()
     }
 });
 
@@ -37,8 +62,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
  * 
  * @param {string} audioSrcPath 
  */
-function playAudio(audioSrcPath)
-{
+function playAudio(audioSrcPath) {
     let audio;
     audio = new Audio();
     audio.src = audioSrcPath;
